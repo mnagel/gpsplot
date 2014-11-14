59cab187141bb2cd98b2e35847e698c6dba6ee36
Michael Nagel <michael.nagel@devzero.de>
2014-11-14 21:33:11 +0100

    add script to gather some demo data

---
+++ init.sh
@@ -0,0 +1,8 @@
+# http://commons.wikimedia.org/wiki/User:XRay
+
+curl "http://upload.wikimedia.org/wikipedia/commons/c/c9/Flumserberg_%28Schweiz%29_--_2011_--_7.jpg" > data/img/cow.jpg
+curl "http://upload.wikimedia.org/wikipedia/commons/1/13/L%C3%BCdinghausen%2C_Burg_Wolfsberg_--_2014_--_1743.jpg" > data/img/house.jpg
+
+# curl "" > data/img/xxx.jpg
+
+
