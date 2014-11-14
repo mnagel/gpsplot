d9b39273cff2cf426a57409df29a0552b669599c
Michael Nagel <michael.nagel@devzero.de>
2014-11-14 22:37:10 +0100

    download more pictures

--- init.sh
+++ init.sh
@@ -3,6 +3,28 @@
 curl "http://upload.wikimedia.org/wikipedia/commons/c/c9/Flumserberg_%28Schweiz%29_--_2011_--_7.jpg" > data/img/cow.jpg
 curl "http://upload.wikimedia.org/wikipedia/commons/1/13/L%C3%BCdinghausen%2C_Burg_Wolfsberg_--_2014_--_1743.jpg" > data/img/house.jpg
 
+curl "http://upload.wikimedia.org/wikipedia/commons/a/a3/Amboy_%28California%2C_USA%29_--_2012_--_4.jpg" > data/img/1.jpg
+curl "http://upload.wikimedia.org/wikipedia/commons/7/7a/Amboy_%28California%2C_USA%29%2C_Hist._Route_66_--_2012_--_1.jpg" > data/img/2.jpg
+curl "http://upload.wikimedia.org/wikipedia/commons/0/0c/Buldern%2C_Schloss_Buldern_--_2.jpg" > data/img/3.jpg
+curl "http://upload.wikimedia.org/wikipedia/commons/d/d8/D%C3%BClmen%2C_Haus_Osthoff_--_2012_--_2-2.jpg" > data/img/4.jpg
+curl "http://upload.wikimedia.org/wikipedia/commons/3/34/D%C3%BClmen%2C_Naturschutzgebiet_-Am_Enteborn-_--_2014_--_0202.jpg" > data/img/5.jpg
+curl "http://upload.wikimedia.org/wikipedia/commons/0/0c/D%C3%BClmen%2C_Nonnenturm_--_2012_--_1.jpg" > data/img/6.jpg
+curl "http://upload.wikimedia.org/wikipedia/commons/a/a6/D%C3%BClmen%2C_Umland%2C_Sonnenaufgang_--_2012_--_8.jpg" > data/img/7.jpg
+curl "http://upload.wikimedia.org/wikipedia/commons/c/cd/D%C3%BClmen%2C_Umland%2C_Sonnenaufgang_--_2012_--_9.jpg" > data/img/8.jpg
+curl "http://upload.wikimedia.org/wikipedia/commons/a/a1/Hiddingsel%2C_St.-Johannes-Nepomuk-Kapelle_--_2014_--_2990.jpg" > data/img/9.jpg
+curl "http://upload.wikimedia.org/wikipedia/commons/7/7a/Hoover_Dam%2C_Nevada_%28Arizona-Nevada%2C_USA%29_--_2012_--_14.jpg" > data/img/11.jpg
+curl "http://upload.wikimedia.org/wikipedia/commons/5/58/Kirchspiel%2C_R%C3%B6dder%2C_M%C3%A4usescheune_--_2014_--_2919.jpg" > data/img/12.jpg
+curl "http://upload.wikimedia.org/wikipedia/commons/f/f9/Kirchspiel%2C_R%C3%B6dder%2C_M%C3%A4usescheune_--_2014_--_2944.jpg" > data/img/13.jpg
+curl "http://upload.wikimedia.org/wikipedia/commons/c/c8/Las_Vegas_%28Nevada%2C_USA%29%2C_The_Strip_--_2012_--_19.jpg" > data/img/14.jpg
+curl "http://upload.wikimedia.org/wikipedia/commons/0/09/Los_Angeles_%28California%2C_USA%29%2C_South_Olive_Street_--_2012_--_7.jpg" > data/img/15.jpg
+curl "http://upload.wikimedia.org/wikipedia/commons/3/3d/L%C3%BCdinghausen%2C_Burg_L%C3%BCdinghausen_--_2014_--_5502.jpg" > data/img/16.jpg
+curl "http://upload.wikimedia.org/wikipedia/commons/1/1a/Merfeld%2C_Wildpferdefang_--_2014_--_0639.jpg" > data/img/17.jpg
+curl "http://upload.wikimedia.org/wikipedia/commons/4/48/M%C3%BCnster%2C_Schloss_--_2014_--_6771.jpg" > data/img/18.jpg
+curl "http://upload.wikimedia.org/wikipedia/commons/f/f8/M%C3%BCnster%2C_LVM%2C_-Villa_Kunterbunt-_--_2013_--_1-2.jpg" > data/img/21.jpg
+curl "http://upload.wikimedia.org/wikipedia/commons/7/76/M%C3%BCnster%2C_Westdeutsche_Lotterie_--_2013_--_1-4.jpg" > data/img/22.jpg
+curl "http://upload.wikimedia.org/wikipedia/commons/e/e4/M%C3%BCnster%2C_WestLotto_--_2013_--_1-3.jpg" > data/img/23.jpg
+curl "http://upload.wikimedia.org/wikipedia/commons/7/72/Paris%2C_Notre_Dame_--_2014_--_1458.jpg" > data/img/24.jpg
+curl "http://upload.wikimedia.org/wikipedia/commons/0/02/Rome_%28Italy%29%2C_Piazza_della_Rotonda%2C_Hub_of_a_Coach_--_2013_--_10.jpg" > data/img/25.jpg
+curl "http://upload.wikimedia.org/wikipedia/commons/e/e1/Senden%2C_Dortmund-Ems-Kanal_--_2014_--_2991.jpg" > data/img/26.jpg
+
 # curl "" > data/img/xxx.jpg
-
-
