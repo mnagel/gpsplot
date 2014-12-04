b91101f10b589f58e137fe732ac744e109a9bf4e
Fabian Knittel <fabian.knittel@lettink.de>
2014-12-04 21:07:16 +0100

    Fix init.sh to work with empty directories

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
+#!/bin/sh
 # http://commons.wikimedia.org/wiki/User:XRay
 
+mkdir -p data/img
 curl "http://upload.wikimedia.org/wikipedia/commons/c/c9/Flumserberg_%28Schweiz%29_--_2011_--_7.jpg" > data/img/cow.jpg
 curl "http://upload.wikimedia.org/wikipedia/commons/1/13/L%C3%BCdinghausen%2C_Burg_Wolfsberg_--_2014_--_1743.jpg" > data/img/house.jpg
 
