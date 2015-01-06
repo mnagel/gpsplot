33373d132c632679cf8bb76ad5c719b7bbc5e223
Michael Nagel <michael.nagel@devzero.de>
2015-01-07 00:31:13 +0100

    massively dirty historgram

--- gpsplot.js
+++ gpsplot.js
@@ -184,7 +184,13 @@ function main(pin_dtos) {
     sliderControl.startSlider();
 
     //console.log(listOfMarkers);
-    //calculateTimeBuckets(listOfMarkers);
+    buckets = calculateTimeBuckets(listOfMarkers);
+
+        console.log("a");
+    console.log(document.getElementById("example"))
+    basic_time(document.getElementById("example"), buckets);
+     console.log("b");
+
 }
 
 function bucketIdFor(marker) {
@@ -207,4 +213,75 @@ function calculateTimeBuckets(markers) {
   });
   console.log(buckets);
   return buckets;
+}
+
+
+function basic_time(container, buckets) {
+
+var keys = [];
+for (var key in buckets) {
+  if (buckets.hasOwnProperty(key)) {
+    keys.push(key);
+  }
+}
+
+keys = keys.sort();
+
+  var
+    d1    = [],
+    start = new Date("2009/01/01 01:00").getTime(),
+    options,
+    graph,
+    i, x, o;
+
+  for (i = 0; i < keys.length; i++) { // TODO nicer loop
+    // x = start+(i*1000*3600*24*36.5);
+    x = buckets[keys[i]][0].pin.date; // TODO hacky
+    var y = buckets[keys[i]].length;
+    d1.push([x, y]);
+  }
+        
+  options = {
+    xaxis : {
+      mode : 'time', 
+      labelsAngle : 45
+    },
+    mouse : {
+        track : true,
+        relative : true
+      },
+    selection : {
+      mode : 'x'
+    },
+    HtmlText : false,
+    title : 'Time',
+              
+  };
+        
+  // Draw graph with default options, overwriting with passed options
+  function drawGraph (opts) {
+
+    // Clone the options, so the 'options' variable always keeps intact.
+    o = Flotr._.extend(Flotr._.clone(options), opts || {});
+
+    // Return a new graph.
+    return Flotr.draw(
+      container,
+      [ d1 ],
+      o
+    );
+  }
+
+  graph = drawGraph();      
+        
+  Flotr.EventAdapter.observe(container, 'flotr:select', function(area){
+    // Draw selected area
+    graph = drawGraph({
+      xaxis : { min : area.x1, max : area.x2, mode : 'time', labelsAngle : 45 },
+      yaxis : { min : area.y1, max : area.y2 }
+    });
+  });
+        
+  // When graph is clicked, draw the graph with default area.
+  Flotr.EventAdapter.observe(container, 'flotr:click', function () { graph = drawGraph(); });
 }
