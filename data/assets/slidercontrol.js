7e7689b80c87c60e697b4f80888a6db832c639ae
Fabian Knittel <fabian.knittel@lettink.de>
2015-01-07 00:10:41 +0100

    Simplify slidercontrol

--- data/assets/slidercontrol.js
+++ data/assets/slidercontrol.js
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2013 Dennis Wilhelm
+ * Copyright (c) 2013  Dennis Wilhelm
+ * Copyright (C) 2015  Fabian Knittel <fabian.knittel@lettink.de>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -19,140 +20,109 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  *
- * See https://github.com/dwilhelm89/LeafletSlider
+ * Based on https://github.com/dwilhelm89/LeafletSlider
  */
 
 L.Control.SliderControl = L.Control.extend({
     options: {
         position: 'topright',
-        layers: null,
+        layer: null,
+        range: false,
+        follow: false,
+
+        markers: [],
         maxValue: -1,
         minValue: -1,
-        markers: null,
-        range: false,
-        follow: false
-    },
-
-    initialize: function (options) {
-        L.Util.setOptions(this, options);
-        this._layer = this.options.layer;
-
-    },
-
-    setPosition: function (position) {
-        var map = this._map;
-
-        if (map) {
-            map.removeControl(this);
-        }
-
-        this.options.position = position;
-
-        if (map) {
-            map.addControl(this);
-        }
-        this.startSlider();
-        return this;
     },
 
     onAdd: function (map) {
-        this.options.map = map;
+        var options = this.options;
+        options.map = map;
 
         // Create a control sliderContainer with a jquery ui slider
         var sliderContainer = L.DomUtil.create('div', 'slider', this._container);
-        $(sliderContainer).append('<div id="leaflet-slider" style="width:200px"><div class="ui-slider-handle"></div><div id="slider-timestamp" style="width:200px; margin-top:10px;background-color:#FFFFFF"></div></div>');
-        //Prevent map panning/zooming while using the slider
-        $(sliderContainer).mousedown(function () {
-            map.dragging.disable();
-        });
-        $(document).mouseup(function () {
-            map.dragging.enable();
-            //Only show the slider timestamp while using the slider
-            $('#slider-timestamp').html('');
-        });
+        var leafletSlider = $.parseHTML('<div id="leaflet-slider" style="width:200px"><div class="ui-slider-handle"></div></div>');
+        $(sliderContainer).append(leafletSlider);
+        var sliderTimestamp = $.parseHTML('<div id="slider-timestamp" style="width:200px; margin-top:10px;background-color:#FFFFFF"></div>');
+        $(leafletSlider).append(sliderTimestamp);
 
-        var options = this.options;
-        this.options.markers = [];
-
-        //If a layer has been provided: calculate the min and max values for the slider
-        if (this._layer) {
-            this._layer.eachLayer(function (layer) {
+        if (options.layer) {
+            options.layer.eachLayer(function (layer) {
                 if (options.minValue === -1) {
                     options.minValue = layer._leaflet_id;
                 }
                 options.maxValue = layer._leaflet_id;
                 options.markers[layer._leaflet_id] = layer;
             });
-            this.options = options;
         } else {
             console.log("Error: You have to specify a layer via new SliderControl({layer: your_layer});");
         }
-        return sliderContainer;
-    },
 
-    onRemove: function (map) {
-        //Delete all markers which where added via the slider and remove the slider div
-        for (i = this.options.minValue; i < this.options.maxValue; i++) {
-            map.removeLayer(this.options.markers[i]);
-        }
-        $('#leaflet-slider').remove();
-    },
-
-    startSlider: function () {
-        _options = this.options;
-        $("#leaflet-slider").slider({
-            range: _options.range,
-            value: _options.minValue + 1,
-            min: _options.minValue,
-            max: _options.maxValue +1,
+        $(leafletSlider).slider({
+            range: options.range,
+            value: options.minValue + 1,
+            min: options.minValue,
+            max: options.maxValue +1,
             step: 1,
+            start: function( event, ui ) {
+                // Prevent map panning/zooming while using the slider
+                map.dragging.disable();
+            },
+            stop: function( event, ui ) {
+                map.dragging.enable();
+                //Only show the slider timestamp while using the slider
+                $(sliderTimestamp).hide();
+            },
             slide: function (e, ui) {
-                var map = _options.map;
-                if(!!_options.markers[ui.value]) {
-                    if (_options.markers[ui.value].pin !== undefined) {
-                        if (_options.markers[ui.value].pin.date){
-                            $('#slider-timestamp').html(_options.markers[ui.value].pin.date.format('Y-m-d H:i:s'));
+                var map = options.map;
+                if(!!options.markers[ui.value]) {
+                    if (options.markers[ui.value].pin !== undefined) {
+                        if (options.markers[ui.value].pin.date){
+                            $(sliderTimestamp).html(options.markers[ui.value].pin.date.format('Y-m-d H:i:s'));
+                            $(sliderTimestamp).show();
                         } else {
                             console.error("You have to have a time property");
                         }
                     }
                     
                     var i;
-                    if(_options.range){
+                    if(options.range){
                         // jquery ui using range
                         for (i = ui.values[0]; i <= ui.values[1]; i++){
-                           if(_options.markers[i]) map.addLayer(_options.markers[i]);
+                           if(options.markers[i]) map.addLayer(options.markers[i]);
                         }
-                        for (i = _options.maxValue; i > ui.values[1]; i--) {
-                            if(_options.markers[i]) map.removeLayer(_options.markers[i]);
+                        for (i = options.maxValue; i > ui.values[1]; i--) {
+                            if(options.markers[i]) map.removeLayer(options.markers[i]);
                         }
-                        for (i = _options.minValue; i < ui.values[0]; i++) {
-                            if(_options.markers[i]) map.removeLayer(_options.markers[i]);
+                        for (i = options.minValue; i < ui.values[0]; i++) {
+                            if(options.markers[i]) map.removeLayer(options.markers[i]);
                         }
-                    }else if(_options.follow){
-                        for (i = _options.minValue; i < (ui.value - _options.follow); i++) {
-                            if(_options.markers[i]) map.removeLayer(_options.markers[i]);
+                    }else if(options.follow){
+                        for (i = options.minValue; i < (ui.value - options.follow); i++) {
+                            if(options.markers[i]) map.removeLayer(options.markers[i]);
                         }
-                        for (i = (ui.value - _options.follow); i < ui.value ; i++) {
-                            if(_options.markers[i]) map.addLayer(_options.markers[i]);
+                        for (i = (ui.value - options.follow); i < ui.value ; i++) {
+                            if(options.markers[i]) map.addLayer(options.markers[i]);
                         }
-                        for (i = ui.value; i <= _options.maxValue; i++) {
-                            if(_options.markers[i]) map.removeLayer(_options.markers[i]);
+                        for (i = ui.value; i <= options.maxValue; i++) {
+                            if(options.markers[i]) map.removeLayer(options.markers[i]);
                         }
                     }else{
                         // jquery ui for point before
-                        for (i = _options.minValue; i <= ui.value ; i++) {
-                            if(_options.markers[i]) map.addLayer(_options.markers[i]);
+                        for (i = options.minValue; i <= ui.value ; i++) {
+                            if(options.markers[i]) map.addLayer(options.markers[i]);
                         }
-                        for (i = (ui.value + 1); i <= _options.maxValue; i++) {
-                            if(_options.markers[i]) map.removeLayer(_options.markers[i]);
+                        for (i = (ui.value + 1); i <= options.maxValue; i++) {
+                            if(options.markers[i]) map.removeLayer(options.markers[i]);
                         }
                     }
                 }
             }
         });
-        _options.map.addLayer(_options.markers[_options.minValue]);
-    }
+        options.map.addLayer(options.markers[options.minValue]);
+
+        return sliderContainer;
+    },
 });
 
 L.control.sliderControl = function (options) {
