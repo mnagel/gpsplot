ebf05aa073463eb9a63492bbfc5ddd0529bc15a6
Michael Nagel <michael.nagel@devzero.de>
2015-01-08 22:46:31 +0100

    improve the image histogram. it is still an ugly hack.

--- gpsplot.js
+++ gpsplot.js
@@ -213,72 +213,64 @@ function calculateTimeBuckets(markers) {
 }
 
 
+// TODO this whole function is one big dirty ugly hack
 function basic_time(container, buckets) {
-
-var keys = [];
-for (var key in buckets) {
-  if (buckets.hasOwnProperty(key)) {
-    keys.push(key);
+  var keys = [];
+  for (var key in buckets) {
+    if (buckets.hasOwnProperty(key)) {
+      keys.push(key);
+    }
   }
-}
 
-keys = keys.sort();
+  keys = keys.sort();
 
-  var
-    d1    = [],
-    start = new Date("2009/01/01 01:00").getTime(),
-    options,
-    graph,
-    i, x, o;
+  var d1 = [], options, graph, i;
 
   for (i = 0; i < keys.length; i++) { // TODO nicer loop
-    // x = start+(i*1000*3600*24*36.5);
-    x = buckets[keys[i]][0].pin.date; // TODO hacky
+    var x = buckets[keys[i]][0].pin.date.getTime(); // TODO hacky
     var y = buckets[keys[i]].length;
     d1.push([x, y]);
   }
-        
+
   options = {
     xaxis : {
-      mode : 'time', 
-      labelsAngle : 45
+      labelsAngle : 0,
+      tickFormatter: function(x) {
+        if(isNaN(x)) {
+          return "";
+        } else {
+          var x = parseInt(x);
+          var myDate = new Date(x);
+          return myDate.format('Y-m-d')
+        }
+      }
+    },
+    yaxis : {
+      min : 0,
+      tickFormatter: function(x) {
+        return Math.floor(x).toString();
+      }
     },
     mouse : {
-        track : true,
-        relative : true
-      },
+      track : true,
+      relative : true
+    },
+    bars : {
+      show : true,
+      horizontal : false,
+      shadowSize : 0,
+      barWidth : 0.5
+    },
     selection : {
       mode : 'x'
     },
     HtmlText : false,
-    title : 'Time',
-              
+    title : 'Picture Count',
   };
-        
-  // Draw graph with default options, overwriting with passed options
-  function drawGraph (opts) {
 
-    // Clone the options, so the 'options' variable always keeps intact.
-    o = Flotr._.extend(Flotr._.clone(options), opts || {});
-
-    // Return a new graph.
-    return Flotr.draw(
-      container,
-      [ d1 ],
-      o
-    );
-  }
-
-  graph = drawGraph();      
-        
-  Flotr.EventAdapter.observe(container, 'flotr:select', function(area){
-    // Draw selected area
-    graph = drawGraph({
-      xaxis : { min : area.x1, max : area.x2, mode : 'time', labelsAngle : 45 },
-      yaxis : { min : area.y1, max : area.y2 }
-    });
-  });
-        
-  // When graph is clicked, draw the graph with default area.
-  Flotr.EventAdapter.observe(container, 'flotr:click', function () { graph = drawGraph(); });
+  Flotr.draw(
+    container,
+    [ d1 ],
+    options
+  );
 }
