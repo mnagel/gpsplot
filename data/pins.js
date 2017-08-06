cbe7feb13f85b33bf3ee102688af8ef3e7818dc0
Fabian Knittel <fabian.knittel@andrena.de>
2017-08-06 19:03:47 +0200

    Add current site sample data
    Properly uses full-sized sample images with exif gps data.
    
    Fixes #56

---
+++ data/pins.js
@@ -0,0 +1,580 @@
+var pin_dtos = [
+    {
+        "thumbnail": {
+            "url": "data/thumbs/5c7731cd0f655b8ca396394eba3b804a.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/1.jpg",
+            "width": 4693,
+            "height": 3129
+        },
+        "timestamp": "2012-07-24T10:26:27",
+        "gps": {
+            "lon": -115.74369166666666,
+            "lat": 34.558475
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/60642e025d2bb401b507a1e1d85e6691.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/17.jpg",
+            "width": 5224,
+            "height": 1404
+        },
+        "timestamp": "2014-05-31T15:06:06",
+        "gps": {
+            "lon": 7.131318333333334,
+            "lat": 51.83499333333334
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/a5c460ee7265e961fc042c15eb447491.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/2.jpg",
+            "width": 5065,
+            "height": 2570
+        },
+        "timestamp": "2012-07-24T10:27:53",
+        "gps": {
+            "lon": -115.743745,
+            "lat": 34.55833333333333
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/75f4a448d24b6cc7ec3d94b1de4cf652.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/24.jpg",
+            "width": 19300,
+            "height": 3252
+        },
+        "timestamp": "2014-07-08T17:40:16",
+        "gps": {
+            "lon": 2.3485,
+            "lat": 48.85323666666667
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/213d964cdd5da5bc1786db293abf21f2.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/25.jpg",
+            "width": 5184,
+            "height": 3456
+        },
+        "timestamp": "2013-10-21T14:43:14",
+        "gps": {
+            "lon": 12.476703333333333,
+            "lat": 41.89913833333333
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/664ddf389dc8eaf9d7ed7160f0b41154.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/3.jpg",
+            "width": 5184,
+            "height": 3456
+        },
+        "timestamp": "2013-06-05T07:06:23",
+        "gps": {
+            "lon": 7.374911666666667,
+            "lat": 51.85785333333333
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/283d6bb7106edb8be2fb4c39160cfa8d.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/5.jpg",
+            "width": 5110,
+            "height": 3482
+        },
+        "timestamp": "2014-05-18T07:46:15",
+        "gps": {
+            "lon": 7.209778333333333,
+            "lat": 51.865413333333336
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/f42e5bff6ba84ecbb8475fec58fe7a6d.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/9.jpg",
+            "width": 3124,
+            "height": 4509
+        },
+        "timestamp": "2014-08-01T06:20:18",
+        "gps": {
+            "lon": 7.408346666666667,
+            "lat": 51.83582
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/1c13e52650088db6a1417ee7de7bfe05.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a11.jpg",
+            "width": 5075,
+            "height": 3414
+        },
+        "timestamp": "2014-07-17T12:51:20",
+        "gps": {
+            "lon": 6.958798333333333,
+            "lat": 50.94083333333333
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/60dbd7d2df9a8e31efe0bab12eadf55f.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a12.jpg",
+            "width": 5472,
+            "height": 3648
+        },
+        "timestamp": "2014-08-16T17:39:39",
+        "gps": {
+            "lon": 6.7061416666666664,
+            "lat": 51.946756666666666
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/544acd77a3bd4c740120482274ce1c33.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a13.jpg",
+            "width": 2133,
+            "height": 3200
+        },
+        "timestamp": "2013-11-23T14:43:03",
+        "gps": {
+            "lon": 8.412246666666666,
+            "lat": 49.011291666666665
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/e3dd268996681ec2d7b810a0e95db8f4.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a14.jpg",
+            "width": 4867,
+            "height": 2852
+        },
+        "timestamp": "2012-07-22T16:36:12",
+        "gps": {
+            "lon": -117.157345,
+            "lat": 32.7174
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/7666bf22f529e573fe46e4ccd94e5744.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a15.jpg",
+            "width": 4730,
+            "height": 3069
+        },
+        "timestamp": "2014-07-17T12:51:37",
+        "gps": {
+            "lon": 6.958865,
+            "lat": 50.940865
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/fac68161e294381ccae024e4bd2f3fa8.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a16.jpg",
+            "width": 4557,
+            "height": 3038
+        },
+        "timestamp": "2013-11-23T14:15:51",
+        "gps": {
+            "lon": 8.419046666666667,
+            "lat": 49.01093
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/fa4de1dd7b56e9909d4baafcb70c0fa4.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a17.jpg",
+            "width": 4827,
+            "height": 1956
+        },
+        "timestamp": "2013-11-23T15:03:41",
+        "gps": {
+            "lon": 8.414661666666667,
+            "lat": 49.01108833333333
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/49f63ee049934194214083bd901b2ae6.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a18.jpg",
+            "width": 5472,
+            "height": 3506
+        },
+        "timestamp": "2014-10-04T11:00:25",
+        "gps": {
+            "lon": 6.997463333333333,
+            "lat": 53.67756333333333
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/7b79eee9982299711d560b68f29a88f9.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a19.jpg",
+            "width": 3978,
+            "height": 3180
+        },
+        "timestamp": "2014-08-16T18:49:47",
+        "gps": {
+            "lon": 6.706188333333333,
+            "lat": 51.946781666666666
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/607cdce8d5445fdb06803e435dfae6af.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a2.jpg",
+            "width": 5472,
+            "height": 2683
+        },
+        "timestamp": "2014-10-04T11:15:50",
+        "gps": {
+            "lon": 7.000275,
+            "lat": 53.670825
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/241be04413c631497624f657b826ee3e.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a20.jpg",
+            "width": 3200,
+            "height": 2040
+        },
+        "timestamp": "2013-11-02T21:59:43",
+        "gps": {
+            "lon": 7.200191666666667,
+            "lat": 51.611641666666664
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/80c7f66ab42bc0b7ea8bbdf497e48ac7.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a21.jpg",
+            "width": 3160,
+            "height": 3840
+        },
+        "timestamp": "2014-07-17T17:28:51",
+        "gps": {
+            "lon": 6.961938333333333,
+            "lat": 50.94079
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/5a8cf06b82c39078b4e038bb70dd2720.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a22.jpg",
+            "width": 2604,
+            "height": 3906
+        },
+        "timestamp": "2013-11-23T14:17:12",
+        "gps": {
+            "lon": 8.41887,
+            "lat": 49.01031166666667
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/8400668eba75436c47720559df501aef.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a23.jpg",
+            "width": 5299,
+            "height": 2337
+        },
+        "timestamp": "2014-10-04T11:02:06",
+        "gps": {
+            "lon": 6.995521666666667,
+            "lat": 53.67658333333333
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/757ebf242577f204c590d5333d59c89f.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a24.jpg",
+            "width": 3648,
+            "height": 5472
+        },
+        "timestamp": "2014-10-04T11:20:14",
+        "gps": {
+            "lon": 6.997733333333334,
+            "lat": 53.67178666666667
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/44b307b63c2ddabccea2bbe700960dcc.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a25.jpg",
+            "width": 3189,
+            "height": 4805
+        },
+        "timestamp": "2013-11-02T21:34:17",
+        "gps": {
+            "lon": 7.197596666666667,
+            "lat": 51.61521333333334
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/b9635bbcdd1d679828a53dd30afa52dd.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a26.jpg",
+            "width": 3447,
+            "height": 4898
+        },
+        "timestamp": "2012-07-22T13:32:02",
+        "gps": {
+            "lon": -117.157345,
+            "lat": 32.7174
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/85b0c3ed1626d9f850a1f5cbb7ad42cb.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a27.jpg",
+            "width": 5077,
+            "height": 3385
+        },
+        "timestamp": "2013-10-20T11:07:44",
+        "gps": {
+            "lon": 12.4578,
+            "lat": 41.902413333333335
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/bb852a29c21953c5f1ab6c79d8d0b708.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a28.jpg",
+            "width": 2890,
+            "height": 3840
+        },
+        "timestamp": "2014-07-17T12:55:15",
+        "gps": {
+            "lon": 6.96163,
+            "lat": 50.94079166666666
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/633840d2e48037dc843356ae6d173cea.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a3.jpg",
+            "width": 5472,
+            "height": 3648
+        },
+        "timestamp": "2014-09-09T12:20:27",
+        "gps": {
+            "lon": 7.6139399999999995,
+            "lat": 51.94992333333333
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/4d5556e73b70531ed62611d52635ee4e.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a4.jpg",
+            "width": 4334,
+            "height": 3374
+        },
+        "timestamp": "2014-10-10T19:13:46",
+        "gps": {
+            "lon": 7.27538,
+            "lat": 51.83103666666667
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/e453718d58e9ba844292c9d00c769ad9.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a5.jpg",
+            "width": 5472,
+            "height": 3648
+        },
+        "timestamp": "2014-11-14T14:42:08",
+        "gps": {
+            "lon": 7.614538333333334,
+            "lat": 51.944925
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/90b9376213da9a4498c4c6610569c69d.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a7.jpg",
+            "width": 3840,
+            "height": 2670
+        },
+        "timestamp": "2014-08-09T10:52:03",
+        "gps": {
+            "lon": 7.442866666666667,
+            "lat": 51.775905
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/53d797ac0d19ee15152fbac02f2eddd9.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a8.jpg",
+            "width": 2459,
+            "height": 4183
+        },
+        "timestamp": "2013-11-23T15:01:19",
+        "gps": {
+            "lon": 8.41308,
+            "lat": 49.0113
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/df4eb51632b2bfc2bcb2fb30d9dcd8bd.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/a9.jpg",
+            "width": 5093,
+            "height": 3109
+        },
+        "timestamp": "2012-07-22T16:38:24",
+        "gps": {
+            "lon": -117.157345,
+            "lat": 32.7174
+        }
+    },
+    {
+        "thumbnail": {
+            "url": "data/thumbs/d2346f1d6bced8dd87df7685311e3345.jpg"
+        },
+        "comment": "",
+        "image": {
+            "rotation": 1,
+            "url": "data/img/house.jpg",
+            "width": 5320,
+            "height": 3017
+        },
+        "timestamp": "2014-07-12T10:18:47",
+        "gps": {
+            "lon": 7.444165,
+            "lat": 51.769125
+        }
+    }
+];
